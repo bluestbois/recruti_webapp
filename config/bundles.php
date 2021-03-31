6da2baf03ca6cf6007b0cd9c45b09112fefc2610
SaddemAmine <naceuramine.saddem@esprit.tn>
2021-03-31 15:46:04 +0200

    Added 404 and 403 pages
    And a few minor changes.

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
 ];
